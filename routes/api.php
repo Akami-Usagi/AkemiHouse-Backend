526979783050bf853808218ad5dde01b4e118527
Camilo Arango <ccarango17@soy.sena.edu.co>
2024-11-24 16:45:41 -0500

    patch in product mod

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\CategoryController;
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{id}', [ProductController::class, 'show']);
 Route::post('/products', [ProductController::class, 'store']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
+Route::patch('/products/{id}', [ProductController::class, 'update']);
 
 
 Route::get('/categories', [CategoryController::class, 'index']);
